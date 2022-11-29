d00bdbd543a4c103aaab9d2d6f7b94f65f107ac9
KirillovaD <sdaria@inbox.ru>
2022-11-29 13:13:06 +0300

    add import l9

--- src/index.tsx
+++ src/index.tsx
@@ -8,12 +8,38 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <App/>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// let users = [
+//     {
+//         name: "Daria",
+//         age: 36,
+//         address: {
+//             city: "Moscow",
+//             country: "Russia"
+//         }
+//     },
+//     {
+//         name: "Kirill",
+//         age: 38,
+//         address: {
+//             city: "Moscow",
+//             country: "Russia"
+//         }
+//     }
+// ]
+
+
+// let a: Array <number>;
+// a= [1,3,4,5,7,8,9];
+// a= [];
+// a = ["dfadf","fdfdf"]
+// a = [false, true]
+// console.log(users[0].address.city)
